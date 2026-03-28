e523138b377d7ad3b568d63617c92ca70e49a036
aspsptyd <devadiroot@gmail.com>
2026-03-28 20:29:43 +0700

    Add: module add to cart with qty update with useState (React Hooks)

--- src/components/pages/products.jsx
+++ src/components/pages/products.jsx
@@ -60,13 +60,13 @@ const ProductsPage = () => {
     }
 
     const handleAddToCart = (id) => {
-        setCart([
-            ...cart, // Spreed Operator
-            {
-                id,
-                qty: 1,
-            }
-        ])
+        if(cart.find((item) => item.id === id)) {
+            setCart(
+                cart.map((item) => item.id === id ? {...item, qty: item.qty + 1} : item)
+            )
+        } else {
+            setCart([...cart, {id, qty: 1}])
+        }
     }
 
     return (
